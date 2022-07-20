316e5bb036168cd9e91a45231492298448e885c3
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-20 10:57:40 +0100

    import display function from modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import _ from "lodash";
 import "./style.css";
+import { displayLeaderScores } from "./modules/addScore";
+
+const submitBtn = document.querySelector("#submit");
+displayLeaderScores;
