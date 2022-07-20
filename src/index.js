4123f60764e2c4a7efc1244ce68f548561e9ab71
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-20 11:27:54 +0100

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import _ from "lodash";
-import "./style.css";
-import { displayLeaderScores } from "./modules/addScore";
+import _ from 'lodash';
+import './style.css';
+import displayLeaderScores from './modules/addScore.js';
 
-const submitBtn = document.querySelector("#submit");
-displayLeaderScores;
-submitBtn.addEventListener("click", displayLeaderScores);
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener('click', displayLeaderScores);
--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,15 +1,17 @@
-export const displayLeaderScores = (e) => {
+const displayLeaderScores = (e) => {
   e.preventDefault();
-  const userName = document.querySelector("#user_name");
-  const userScore = document.querySelector("#user_score");
-  const leaderScores = document.querySelector(".leader-scores");
-  const listScores = document.createElement("li");
-  const name = document.createElement("span");
-  const score = document.createElement("span");
-  score.classList.add("scores");
+  const userName = document.querySelector('#user_name');
+  const userScore = document.querySelector('#user_score');
+  const leaderScores = document.querySelector('.leader-scores');
+  const listScores = document.createElement('li');
+  const name = document.createElement('span');
+  const score = document.createElement('span');
+  score.classList.add('scores');
   name.innerHTML = `${userName.value}`;
   score.innerHTML = `${userScore.value}`;
   listScores.appendChild(name);
   listScores.appendChild(score);
   leaderScores.appendChild(listScores);
 };
+
+module.exports = displayLeaderScores;
