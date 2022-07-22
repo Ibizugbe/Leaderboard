a0de1c8031965e61c1129221f104e225ef5006f0
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-22 13:42:58 +0100

    add bootstrap to project

--- src/modules/displayLeaderScores.js
+++ src/modules/displayLeaderScores.js
@@ -1,10 +1,15 @@
-const leaderScores = document.querySelector('.leader-scores');
+const leaderScores = document.querySelector(".leader-scores");
 const displayLeaderScores = (myData) => {
-  leaderScores.innerHTML = '';
+  leaderScores.innerHTML = "";
   myData.sort((a, b) => a.score - b.score);
   myData.forEach((data) => {
-    const listScores = `<li class="score-li">${data.user}: ${data.score}</li>`;
-    leaderScores.insertAdjacentHTML('beforeend', listScores);
+    const listScores = `
+    <tr>
+      <td>${data.user}</td>
+      <td>${data.score}</td>
+    </tr>
+    `;
+    leaderScores.insertAdjacentHTML("beforeend", listScores);
   });
 };
 
